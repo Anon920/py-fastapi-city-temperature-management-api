c2c25cbbc8c9a20a55058e2f9ffcbd8cd85d7898
Dima Drozdenko <anonimchik920@gmail.com>
2024-11-28 16:03:57 +0200

    Solution

--- main.py
+++ main.py
@@ -23,17 +23,17 @@ async def root() -> dict:
 
 
 # City
-@app.post("/cities", response_model=schemas.City)
+@app.post("/cities/", response_model=schemas.City)
 def create_city(city: schemas.CityCreate, db: Session = Depends(get_db)):
     return crud.create_city(db=db, city=city)
 
 
-@app.get("/cities", response_model=list[schemas.City])
+@app.get("/cities/", response_model=list[schemas.City])
 async def get_cities(db: Session = Depends(get_db)):
     return crud.get_cities(db)
 
 
-@app.get("/cities/{city_id}", response_model=schemas.City)
+@app.get("/cities/{city_id}/", response_model=schemas.City)
 async def get_city(city_id: int, db: Session = Depends(get_db)):
     city = crud.get_city_by_id(db=db, city_id=city_id)
     if city is None:
@@ -41,7 +41,7 @@ async def get_city(city_id: int, db: Session = Depends(get_db)):
     return city
 
 
-@app.put("/cities/{city_id}", response_model=schemas.City)
+@app.put("/cities/{city_id}/", response_model=schemas.City)
 def update_city(city_id: int, city: schemas.CityCreate, db: Session = Depends(get_db)):
     db_city = crud.update_city(db=db, city_id=city_id, city=city)
     if db_city is None:
@@ -49,7 +49,7 @@ def update_city(city_id: int, city: schemas.CityCreate, db: Session = Depends(ge
     return db_city
 
 
-@app.delete("/cities/{city_id}", response_model=schemas.City)
+@app.delete("/cities/{city_id}/", response_model=schemas.City)
 def delete_city(city_id: int, db: Session = Depends(get_db)):
     db_city = crud.delete_city(db=db, city_id=city_id)
     if db_city is None:
@@ -58,7 +58,7 @@ def delete_city(city_id: int, db: Session = Depends(get_db)):
 
 
 # Temperature
-@app.post("/temperatures/update")
+@app.post("/temperatures/update/")
 async def update_temperatures(db: Session = Depends(get_db)):
     cities = crud.get_cities(db)
     for city in cities:
